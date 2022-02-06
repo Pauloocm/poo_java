8506769cd8d33a55a63ae396af0a52d0ac892ac1
Pauloocm <paulooo.cm@gmail.com>
2022-02-06 11:50:10 -0300

    poojava

---
+++ poojava/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,15 @@
+
+package javaapplication1;
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.tampada = false;
+        c1.status();
+    }
+    
+}
+ 
