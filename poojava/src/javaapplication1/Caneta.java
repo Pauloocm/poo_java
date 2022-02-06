79439e8d03104952848e766ea81491e224772f67
Pauloocm <paulooo.cm@gmail.com>
2022-02-06 12:08:38 -0300

    teste

--- poojava/src/javaapplication1/Caneta.java
+++ poojava/src/javaapplication1/Caneta.java
@@ -8,18 +8,24 @@ public class Caneta {
     int carga;
     boolean tampada;
     void status(){
-        System.out.println("Uma caneta" + this.cor);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
         System.out.println("Está tampada? " + this.tampada);
     }
     void rabiscar(){
-        
+        if (tampada == false){
+            System.out.println("Estou rabiscando");
+        }else{
+            System.out.println("Erro: a caneta está tampada");
+        }
     }
     
     void tampar(){
-        
+        tampada = true;
     }
     
     void destampar(){
-        
+        tampada = false;
     }
 }
