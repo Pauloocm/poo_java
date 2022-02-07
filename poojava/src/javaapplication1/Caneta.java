2e13ffc609441f72dcc9521019f1be6b556be582
Pauloocm <paulooo.cm@gmail.com>
2022-02-06 22:36:36 -0300

    classes privadas ou não

--- poojava/src/javaapplication1/Caneta.java
+++ poojava/src/javaapplication1/Caneta.java
@@ -2,18 +2,17 @@
 package javaapplication1;
 
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
-    void status(){
-        System.out.println("Modelo: " + this.modelo);
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    protected boolean tampada;
+    public void status(){
         System.out.println("Uma caneta: " + this.cor);
         System.out.println("Ponta: " + this.ponta);
         System.out.println("Está tampada? " + this.tampada);
     }
-    void rabiscar(){
+    public void rabiscar(){
         if (tampada == false){
             System.out.println("Estou rabiscando");
         }else{
@@ -21,11 +20,11 @@ public class Caneta {
         }
     }
     
-    void tampar(){
+    protected void tampar(){
         tampada = true;
     }
     
-    void destampar(){
+    protected void destampar(){
         tampada = false;
     }
 }
--- poojava/src/javaapplication1/JavaApplication1.java
+++ poojava/src/javaapplication1/JavaApplication1.java
@@ -6,9 +6,10 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         Caneta c1 = new Caneta();
         c1.cor = "Azul";
-        c1.ponta = 0.5f;
-        c1.tampada = false;
+        c1.ponta = 0.5;
+        c1.tampada = true;
         c1.status();
+        c1.rabiscar();
     }
     
 }
